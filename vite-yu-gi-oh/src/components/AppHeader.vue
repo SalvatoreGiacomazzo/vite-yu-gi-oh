5b2e4e83f4dfd84a758cad2211f197d5f9ff5c5e
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-09-19 17:34:06 +0200

    first commit

---
+++ vite-yu-gi-oh/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <header class="text-warning text-center">
+        <h1>Yu-Gi-Oh API</h1>
+    </header>
+</template>
+
+<style lang="scss">
+header {
+    background-color: #40151D;
+    height: 100px;
+}
+</style>
